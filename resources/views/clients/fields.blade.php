a70fd134d352fd63bbb35e2d827bdb69a98ba2ee
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2018-04-16 09:31:56 +0800

    display name change

--- resources/views/clients/fields.blade.php
+++ resources/views/clients/fields.blade.php
@@ -1,3 +1,17 @@
+  @if(Request::is('profile/*') && Auth::user()->getOriginal()['role'] == "client")
+  <div class="form-group">
+    {{ Form::label('firstname', 'First Name: ') }}
+    {{ Form::text('firstname', null, ['class' => 'form-control', 'readonly' => 'readonly']) }}
+  </div>
+  <div class="form-group">
+    {{ Form::label('middlename', 'Middle Name: ') }}
+    {{ Form::text('middlename', null, ['class' => 'form-control', 'readonly' => 'readonly']) }}
+  </div>
+  <div class="form-group">
+    {{ Form::label('lastname', 'Last Name: ') }}
+    {{ Form::text('lastname', null, ['class' => 'form-control', 'readonly' => 'readonly']) }}
+  </div>
+  @else
   <div class="form-group">
     {{ Form::label('firstname', 'First Name: ') }}
     {{ Form::text('firstname', null, ['class' => 'form-control']) }}
@@ -10,6 +24,8 @@
     {{ Form::label('lastname', 'Last Name: ') }}
     {{ Form::text('lastname', null, ['class' => 'form-control']) }}
   </div>
+  @endif
+
   <div class="form-group">
     {{ Form::label('username', 'Username: ') }}
     {{ Form::text('username', null, ['class' => 'form-control']) }}
@@ -23,10 +39,17 @@
     {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
   </div>
   @if(Auth::user()->getOriginal()['role'] == "client" || Request::is('clients/*/edit') || Request::is('clients/create'))
+  @if(Request::is('profile/*') && Auth::user()->getOriginal()['role'] == "client")
+  <div class="form-group">
+    {{ Form::label('birthdate', 'Birth Date: ') }}
+    {{ Form::text('birthdate', isset($client->birthdate) ? $client->birthdate->format('Y-m-d'): null, ['class' => 'form-control', 'readonly' => 'readonly']) }}
+  </div>
+  @else
   <div class="form-group">
     {{ Form::label('birthdate', 'Birth Date: ') }}
     {{ Form::text('birthdate', isset($client->birthdate) ? $client->birthdate->format('Y-m-d'): null, ['class' => 'form-control datepicker']) }}
   </div>
+  @endif
   <div class="form-group">
     {{ Form::label('contact_number', 'Contact Number: ') }}
     {{ Form::text('contact_number', null, ['class' => 'form-control']) }}
